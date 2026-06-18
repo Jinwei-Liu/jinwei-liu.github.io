f09eb6147381bc54daab930cb7fac319a1525c9a
Jinwei-Liu <78484191+Jinwei-Liu@users.noreply.github.com>
2026-06-18 10:06:05 +0000

    Deploying to gh-pages from @ Jinwei-Liu/jinwei-liu.github.io@17b6b1842ac1a009e696fda439ed0066849ad356 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -35,16 +35,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-tada-our-paper-legible-shared-autonomy-implicit-communication-of-robot-belief-through-motion-has-been-accepted-to-iros-2026",
+          title: ':tada: Our paper Legible Shared Autonomy: Implicit Communication of Robot Belief Through Motion...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
